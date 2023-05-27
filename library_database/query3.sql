7019d92a6eec321d0f33625585f8be635fe04e10
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-05-27 09:04:41 +0530

    Update query3.sql

--- library_database/query3.sql
+++ library_database/query3.sql
@@ -1,4 +1,4 @@
 /*Delete a book in BOOK table. Update the contents of other tables 
  to reflect this data manipulation operation.*/
 DELETE FROM book
-WHERE book_id = '112';
+WHERE book_id = '114';
