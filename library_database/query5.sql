49e71ddce8f320c19ee4da74b383537fc8fa8d69
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-05-27 07:22:40 +0530

    Update query5.sql

--- library_database/query5.sql
+++ library_database/query5.sql
@@ -1,8 +1,6 @@
 /*Retrieve the details of publisher 
- who published more than 3 books.*/
-SELECT p.name
-FROM publisher p,
-    book b
-WHERE p.name = b.publisher_name
-GROUP BY p.name
-HAVING COUNT(book_id) > 3;
+ who published more than 2 books.*/
+SELECT publisher_name
+FROM book
+GROUP BY publisher_name
+HAVING COUNT(book_id) > 2;
