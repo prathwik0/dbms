7aa713bf1d2057433ee67957ce4ea2599bb4104c
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-05-31 21:45:53 +0530

    Create query2.sql

---
+++ movie_database/query2.sql
@@ -0,0 +1,22 @@
+/*Find the movie names where one or more actors acted in two or more movies*/
+SELECT DISTINCT mov_title
+FROM movie m,
+    movie_cast mc
+WHERE m.mov_id = mc.mov_id
+    AND (
+        SELECT COUNT (mov_id)
+        FROM movie_cast
+        WHERE act_id = mc.act_id
+    ) >= 2;
+
+-- OR
+SELECT DISTINCT mov_title
+FROM movie m,
+    movie_cast mc
+WHERE m.mov_id = mc.mov_id
+    AND mc.act_id IN (
+        SELECT act_id
+        FROM movie_cast
+        GROUP BY act_id
+        HAVING COUNT(mov_id) >= 2
+    );
