dd00f3ccabe82a5ea21ed44b6966880a1bad3c94
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-05-31 21:45:59 +0530

    Create query5.sql

---
+++ movie_database/query5.sql
@@ -0,0 +1,10 @@
+/*Update the rating of all movies directed by ‘Ridley Scott’ to 5.*/
+UPDATE rating
+SET stars = 5
+WHERE mov_id IN (
+        SELECT m.mov_id
+        FROM movie m,
+            director d,
+            WHERE m.dir_id = d.dir_id
+            AND d.dir_name = "Ridley Scott"
+    );
