f721f30c25c3d4511330b87a10af653a6df08019
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-10 00:16:20 +0530

    Update query6.sql

--- movie_database/query6.sql
+++ movie_database/query6.sql
@@ -1,7 +1,7 @@
 /*Find the count of movies released in each year in each language.*/
 SELECT mov_year,
     mov_lang,
-    COUNT(mov_id) COUNT
+    COUNT(mov_id) count_movies
 FROM movie
 GROUP BY mov_year,
     mov_lang
