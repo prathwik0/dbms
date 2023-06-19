44a506f19ae5da90737fc2bc85de3ca89007ef31
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-19 23:34:02 +0530

    Update query1.sql

--- order_database/query1.sql
+++ order_database/query1.sql
@@ -1,5 +1,5 @@
 /* Count the customers with grades above King's Landing average. */
-SELECT COUNT(customer_id)
+SELECT COUNT(customer_id) no_of_customers
 FROM customer c
 WHERE grade > (
         SELECT avg(grade)
