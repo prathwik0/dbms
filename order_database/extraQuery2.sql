4adfe72964fb74847c8ef0915d057f470bbfb896
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-27 01:57:41 +0530

    Update extraQuery2.sql

--- order_database/extraQuery2.sql
+++ order_database/extraQuery2.sql
@@ -3,11 +3,11 @@
  * purchase amount of the orders assigned to him and commission 
  * earned by him.
  */
-SELECT s.salesman_id,
+SELECT s.salesman_id id,
     s.name,
     s.city,
     s.commission,
-    COUNT(*) no_of_orders,
+    COUNT(*) orders,
     SUM(purchase_amt) total_purchase_amt,
     SUM(purchase_amt) * 0.01 * s.commission commission_earned
 FROM salesman s,
